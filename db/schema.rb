7610434072d500a7d12eeb6839ebbfd032f1d94f
Stoyan <stoyan_stoyanov@rocketmail.com>
2024-11-16 21:09:55 +0200

    Add Vote model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_16_152733) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_16_165158) do
   create_table "choices", force: :cascade do |t|
     t.string "content"
     t.string "choosable_type", null: false
@@ -38,4 +38,16 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_16_152733) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "choice_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["choice_id"], name: "index_votes_on_choice_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "votes", "choices"
+  add_foreign_key "votes", "users"
 end
