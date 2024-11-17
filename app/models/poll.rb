02dcd300b9f3665f68280f07a9ddae7728682cc3
Stoyan <stoyan_stoyanov@rocketmail.com>
2024-11-18 00:41:01 +0200

    Validate minimum number of choices for a poll

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -6,7 +6,15 @@ class Poll < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 65_535 }
 
+  validate :options_count
+
   def created_by?(user_id)
     creator.id == user_id
+  end
+
+  private
+
+  def options_count
+    errors.add(:poll, 'must have at least 2 options.') if choices.size < 2
   end
 end
