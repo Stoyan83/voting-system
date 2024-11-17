8d4d67d436757f75b290de88917feba85a91586a
Stoyan <stoyan_stoyanov@rocketmail.com>
2024-11-17 14:16:57 +0200

    Add modal when voted and refactor JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,30 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import "../frontend/index.js"
+
+
+$(document).ready(function () {
+    // Modal
+    const voteModalElement = $('#voteModal')[0];
+    if (voteModalElement) {
+        const voteModal = new bootstrap.Modal(voteModalElement);
+        voteModal.show();
+    }
+
+    // Navbar: React Route
+    $('#switch-view-toggle').on('change', function () {
+        if ($(this).prop('checked')) {
+            window.location.href = '/modern/home';
+        } else {
+            window.location.href = '/';
+        }
+    });
+
+    // Flash messages
+    const flashMessage = $('#flash-message');
+    if (flashMessage.length) {
+        setTimeout(function () {
+            flashMessage.fadeOut();
+        }, 3000);
+    }
+});
