2a4999f1cce28b1dc32a3425fdd2110059d20efd
Stoyan <stoyan_stoyanov@rocketmail.com>
2024-11-17 16:56:22 +0200

    Refactot JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
+
 import "@hotwired/turbo-rails";
 import "./controllers";
 import * as bootstrap from "bootstrap";
 import "../frontend/index.js";
+
 
 $(document).ready(function () {
     // Modal
@@ -10,7 +12,9 @@ $(document).ready(function () {
         const voteModal = new bootstrap.Modal(voteModalElement);
         voteModal.show();
     }
+})
 
+$(document).on('turbo:load', function () {
     // Navbar: React Route
     $('#switch-view-toggle').on('change', function () {
         window.location.href = $(this).prop('checked') ? '/modern/home' : '/';
@@ -35,10 +39,10 @@ $(document).on('click', '.add_fields', function (event) {
 
     $(this).before(newFields);
 
-    $(this).closest('form').find('.new-choice').each(function() {
-        $(this).find('.remove_fields').on('click', function(event) {
+    $(this).closest('form').find('.new-choice').each(function () {
+        $(this).find('.remove_fields').on('click', function (event) {
             event.preventDefault();
-            $(this).closest('.choice-field').remove(); 
+            $(this).closest('.choice-field').remove();
         });
     });
 });
@@ -46,7 +50,7 @@ $(document).on('click', '.add_fields', function (event) {
 // Remove fields on click
 $(document).on('click', '.remove_fields', function (event) {
     event.preventDefault();
-    
+
     if ($(this).closest('.choice-field').hasClass('new-choice')) {
         $(this).closest('.choice-field').remove();
     }
