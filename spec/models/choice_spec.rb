0339daf4a6a15afdf240ecda05825f50f36fdce1
Stoyan <stoyan_stoyanov@rocketmail.com>
2024-11-18 14:25:15 +0200

    Fix some specs

--- spec/models/choice_spec.rb
+++ spec/models/choice_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Choice, type: :model do
-  it { should belong_to(:choosable) }
+  it { should belong_to(:poll) }
+  it { should have_many(:users).through(:votes) }
+
+  it { should validate_presence_of(:content) }
+  it { should validate_length_of(:content).is_at_most(255) }
 end
